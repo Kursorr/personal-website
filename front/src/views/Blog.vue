6393275d2e203b0e99969d4d0cae7fd742922a56
Ravaniss <senechal.max06@gmail.com>
2019-06-21 19:11:02 +0200

    Start blog page

---
+++ front/src/views/Blog.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container blog">
+    <section class="blog">
+      <div class="parallax">
+        <h2><span class="initials">B</span>log</h2>
+      </div>
+    </section>
+
+    <div class="outContent">
+      <div class="content">
+        <section class="infos">
+          <h1>
+            test
+          </h1>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  div.container.blog {
+    margin-top: 0;
+
+    div.parallax {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 300px;
+      width: 100%;
+      background-image: url('../assets/img/write.jpg');
+      background-size: cover;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-attachment: fixed;
+
+      h2 {
+        font-size: 52px;
+        color: #FFF;
+        text-transform: uppercase;
+        margin-top: 50px;
+      }
+    }
+  }
+</style>
