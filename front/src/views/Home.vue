998c8f7d1115c4ea935e3b32c482c99dce684ad3
Ravaniss <senechal.max06@gmail.com>
2019-07-08 22:09:29 +0200

    Styles / Home - Lazy load img

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,30 +1,36 @@
 <template>
   <div class="container home">
-    <div id="bigImg">
-      <div id="overlay">
-        <div id="myself">
-          <h1>
-            <span class="initials home">m</span>
-            <p class="noup">axime</p>
-            <span class="initials home s">s</span>
-            <p>enechal</p>
-          </h1>
-          <h2>{{ $t('home-title') }} <br />freelance</h2>
-          <router-link to="/projects" tag="a" class="btn animated infinite pulse">
-            {{ $t('home-projects') }}
-          </router-link>
-        </div>
+    <v-lazy-image
+      id="homeImg"
+      :src="require(`@/assets/img/backgrounds/background.jpg`)"
+      src-placeholder="https://cdn-images-1.medium.com/max/80/1*xjGrvQSXvj72W4zD6IWzfg.jpeg"
+    />
+    <div id="overlay">
+      <div id="myself">
+        <h1>
+          <span class="initials home">m</span>
+          <p class="noup">axime</p>
+          <span class="initials home s">s</span>
+          <p>enechal</p>
+        </h1>
+        <h2>{{ $t('home-title') }} <br />freelance</h2>
+        <router-link to="/projects" tag="a" class="btn animated infinite pulse">
+          {{ $t('home-projects') }}
+        </router-link>
       </div>
     </div>
-    <Footer></Footer>
   </div>
-
 </template>
 
 <script lang="ts">
   import { Component, Vue } from 'vue-property-decorator'
+  import VLazyImage from 'v-lazy-image'
 
-  @Component
+  @Component({
+    components: {
+      VLazyImage
+    }
+  })
   export default class Home extends Vue {}
 </script>
 
@@ -33,73 +39,78 @@
     height: 100%;
     margin-top: 0;
 
-    div#bigImg {
+    #homeImg {
       height: 100%;
-      background: url(../assets/img/backgrounds/background.jpg) 100% 0 no-repeat;
-      background-size: cover;
+      width: 100%;
+      position: absolute;
+    }
 
-      div#overlay {
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.3);
+    div#overlay {
+      height: 100%;
+      width: 100%;
+      background-color: rgba(0, 0, 0, 0.4);
+      position: absolute;
+      top: 0;
 
-        div#myself {
-          height: 100%;
+      div#myself {
+        height: 90%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        h1 {
+          font-size: 22px;
           display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
+          color: #fff;
+          text-transform: uppercase;
+          text-align: center;
+          font-weight: 100;
+          letter-spacing: 3px;
+          margin-bottom: 10px;
+          z-index: 2;
 
-          h1 {
-            font-size: 22px;
-            display: flex;
-            color: #fff;
+          .noup {
+            text-transform: lowercase;
+          }
+
+          .initials.home.s {
             text-transform: uppercase;
-            text-align: center;
-            font-weight: 100;
-            letter-spacing: 3px;
-            margin-bottom: 10px;
-
-            .noup {
-              text-transform: lowercase;
-            }
-
-            .initials.home.s {
-              text-transform: uppercase;
-              margin-left: 15px;
-            }
+            margin-left: 15px;
           }
+        }
 
-          h2 {
-            color: #fff;
-            font-size: 40px;
-            text-transform: uppercase;
-            letter-spacing: 2px;
-            font-weight: 100;
+        h2 {
+          color: #fff;
+          font-size: 40px;
+          text-transform: uppercase;
+          letter-spacing: 2px;
+          font-weight: 100;
+          z-index: 2;
+        }
+
+        .animated.infinite {
+          animation-iteration-count: infinite;
+        }
+
+        .pulse {
+          animation-name: pulse;
+        }
+
+        .animated {
+          animation-duration: 1s;
+          animation-fill-mode: both;
+        }
+
+        @keyframes pulse {
+          0% {
+            transform: scaleX(1);
           }
-
-          .animated.infinite {
-            animation-iteration-count: infinite;
+          50% {
+            transform: scale3d(1.05, 1.05, 1.05);
           }
-
-          .pulse {
-            animation-name: pulse;
-          }
-
-          .animated {
-            animation-duration: 1s;
-            animation-fill-mode: both;
-          }
-
-          @keyframes pulse {
-            0% {
-              transform: scaleX(1);
-            }
-            50% {
-              transform: scale3d(1.05, 1.05, 1.05);
-            }
-            100% {
-              transform: scaleX(1);
-            }
+          100% {
+            transform: scaleX(1);
           }
         }
       }
